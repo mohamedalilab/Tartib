e0ca6bb54e0a3baf11b9a115f3140a29ce5da32b
mohamedalilab <mohamedali4dev@gmail.com>
2026-04-18 00:37:25 +0200

    fix: remove resolve from vite config and add baseUrl in tsconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,6 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,9 +12,4 @@ export default defineConfig({
     tailwindcss(),
     tsconfigPaths(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
